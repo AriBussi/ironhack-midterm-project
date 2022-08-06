4a1e890347f31ac1206642985907e07c0709ffcf
Ari Tomas Bussi <abussy@gmail.com>
2022-08-06 10:26:44 +0200

    add dynamic detail page

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,32 +1,47 @@
 export const availableProjects = {
     simplify: {
         title: 'Simplify',
-        completedOn: 'June 22, 2021',
+        completedDate: 'June 22, 2021',
         backgroundImage: '../../assets/projects-section/1.jpg',
         area: 'UI Design & App Development',
         description: [
-            'Aliqua id fugiat nostrud irure ex duis ea quis id quis ad et. Sunt qui esse pariatur duis deserunt mollit dolore cillum minim tempor enim. Elit aute irure tempor cupidatat incididunt sint deserunt ut voluptate aute id deserunt nisi. Aliqua id fugiat nostrud irure ex duis ea quis id quis ad et. Sunt qui esse pariatur duis deserunt mollit dolore cillum minim tempor enim. Elit aute irure tempor cupidatat incididunt sint deserunt ut voluptate aute id deserunt nisi.Aliqua id fugiat nostrud irure ex duis ea quis id quis ad et. Sunt quix duis ea quis id quis ad et. Sunt qui esse pariatur duis deserunt mollit dolore cillum minim tempor enim. Elit aute irure tempor cupidatat incididunt sint deserunt ut voluptate aute id deserunt nisi.',
-            'Aliqua id fugiat nostrud irure ex duis ea quis id quis ad et. Sunt qui esse pariatur duis deserunt mollit dolore cillum minim tempor enim. Elit aute irure tempor cupidatat incididunt sint deserunt ut voluptate aute id deserunt nisi.Aliqua id fugiat nostrud irure ex duis ea quis id quis ad e dolore cillum minim tempor enim. Elit aute irure tempor cupidatat incididunt sint deserunt ut voluptate aute id deserunt nisis ea quis id quis ad et. Sunt qui esse pariatur duis deserunt mollit dolore cillum minim tempor enim. Elit aute irure tempor cupidatat incididunt sint deserunt ut voluptate aute id deserunt nisi.Aliqua id fugiat nostrud irure ex duis ea quis id quis ad e dolore cillum minim tempor enim. Elit aute irure tempor cupidatat incididunt sint deserunt ut voluptate aute id deserunt nis cillum minim tempor enim.'
+            `Eum repudiandae culpa perferendis saepe nihil consectetur sapiente, porro ducimus ipsum dolorum, debitis amet magni accusantium quod exercitationem alias expedita sit earum!
+            Vitae iusto cupiditate earum dolorem itaque distinctio animi sequi error, perferendis nemo at adipisci odio beatae, reprehenderit ad. Porro molestiae assumenda dolor quas alias voluptatem quo harum accusamus quibusdam tempora!
+            Dolorem voluptatum laudantium nostrum natus magnam, animi enim eligendi odio iusto, in doloribus rerum maiores illum cum dolores? Nam perspiciatis est veritatis rerum saepe laborum quo ipsam odio ad sed.`,
+
+            `Neque soluta provident pariatur possimus praesentium, minus dolorem, ad deserunt tempora aliquam blanditiis cumque laudantium eaque omnis. Fugit officia delectus optio voluptates.
+            Odio, quibusdam obcaecati ratione, pariatur minus vero modi voluptates necessitatibus ex itaque ab inventore illum perspiciatis assumenda? Magnam, pariatur? Ipsam incidunt a possimus repellat perspiciatis ipsum sapiente id, sit natus.
+            Pariatur, quae temporibus vel magnam ipsam repellendus ut molestias hic fuga! Fugit ad cum accusamus exercitationem et eaque ducimus error in! Placeat doloribus harum amet quia illum repudiandae, culpa accusamus.`
         ]
     },
     dashcoin: {
         title: 'Dashcoin',
-        completedOn: 'December 2, 2021',
+        completedDate: 'December 2, 2021',
         backgroundImage: '../../assets/projects-section/2.jpg',
         area: 'Web Development',
         description: [
-            'Aliqua id fugiat nostrud irure ex duis ea quis id quis ad et. Sunt qui esse pariatur duis deserunt mollit dolore cillum minim tempor enim. Elit aute irure tempor cupidatat incididunt sint deserunt ut voluptate aute id deserunt nisi. Aliqua id fugiat nostrud irure ex duis ea quis id quis ad et. Sunt qui esse pariatur duis deserunt mollit dolore cillum minim tempor enim. Elit aute irure tempor cupidatat incididunt sint deserunt ut voluptate aute id deserunt nisi.Aliqua id fugiat nostrud irure ex duis ea quis id quis ad et. Sunt quix duis ea quis id quis ad et. Sunt qui esse pariatur duis deserunt mollit dolore cillum minim tempor enim. Elit aute irure tempor cupidatat incididunt sint deserunt ut voluptate aute id deserunt nisi.',
-            'Aliqua id fugiat nostrud irure ex duis ea quis id quis ad et. Sunt qui esse pariatur duis deserunt mollit dolore cillum minim tempor enim. Elit aute irure tempor cupidatat incididunt sint deserunt ut voluptate aute id deserunt nisi.Aliqua id fugiat nostrud irure ex duis ea quis id quis ad e dolore cillum minim tempor enim. Elit aute irure tempor cupidatat incididunt sint deserunt ut voluptate aute id deserunt nisis ea quis id quis ad et. Sunt qui esse pariatur duis deserunt mollit dolore cillum minim tempor enim. Elit aute irure tempor cupidatat incididunt sint deserunt ut voluptate aute id deserunt nisi.Aliqua id fugiat nostrud irure ex duis ea quis id quis ad e dolore cillum minim tempor enim. Elit aute irure tempor cupidatat incididunt sint deserunt ut voluptate aute id deserunt nis cillum minim tempor enim.'
+            `Obcaecati qui excepturi expedita voluptates corrupti facilis ex, ullam quibusdam. In hic perspiciatis inventore exercitationem voluptate maxime officia, totam dignissimos ratione sapiente!
+            Quam harum asperiores exercitationem adipisci dolor illum, facilis corrupti eaque enim ab doloribus dignissimos! Cupiditate, distinctio praesentium rem perferendis dicta nesciunt sint velit obcaecati placeat ea ut tempora eveniet suscipit?`,
+            
+            `Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil tenetur blanditiis maxime necessitatibus nesciunt debitis quas eum repellat quasi natus delectus maiores excepturi ratione voluptatibus distinctio, fugiat perspiciatis magnam nemo.
+            Nihil a natus odit ratione earum hic accusantium esse. Eos odit assumenda perspiciatis. Suscipit rem id fugiat reiciendis voluptatum! Maiores fugit cupiditate rerum earum ipsa dignissimos incidunt inventore quo sequi!
+            Modi nesciunt illo dignissimos, eaque, eligendi corporis aperiam accusamus ducimus necessitatibus, tempore facilis optio cumque fuga quo earum praesentium maxime! Dicta sint iste eaque necessitatibus perspiciatis magni quidem officia cumque?
+            Iure, similique? Corporis ipsa cum qui dicta nobis unde ex. Quam, nesciunt? Consequuntur qui assumenda nesciunt harum sed culpa, laboriosam ipsum nisi velit necessitatibus sapiente blanditiis molestias praesentium impedit fuga?
+            Nostrum quibusdam maxime at, nemo aperiam saepe voluptatum ipsa cupiditate quia, fugit consectetur quae impedit! Soluta porro, earum neque molestiae laborum culpa nemo, autem atque qui impedit rerum, dolores quod.`
         ]
     },
     vectorify: {
         title: 'Vectorify',
-        completedOn: 'August 7, 2021',
-        backgroundImage: '../../assets/projects-section/1.jpg',
+        completedDate: 'August 7, 2021',
+        backgroundImage: '../../assets/projects-section/3.jpg',
         area: 'User Experience Design',
         description: [
-            'Aliqua id fugiat nostrud irure ex duis ea quis id quis ad et. Sunt qui esse pariatur duis deserunt mollit dolore cillum minim tempor enim. Elit aute irure tempor cupidatat incididunt sint deserunt ut voluptate aute id deserunt nisi. Aliqua id fugiat nostrud irure ex duis ea quis id quis ad et. Sunt qui esse pariatur duis deserunt mollit dolore cillum minim tempor enim. Elit aute irure tempor cupidatat incididunt sint deserunt ut voluptate aute id deserunt nisi.Aliqua id fugiat nostrud irure ex duis ea quis id quis ad et. Sunt quix duis ea quis id quis ad et. Sunt qui esse pariatur duis deserunt mollit dolore cillum minim tempor enim. Elit aute irure tempor cupidatat incididunt sint deserunt ut voluptate aute id deserunt nisi.',
-            'Aliqua id fugiat nostrud irure ex duis ea quis id quis ad et. Sunt qui esse pariatur duis deserunt mollit dolore cillum minim tempor enim. Elit aute irure tempor cupidatat incididunt sint deserunt ut voluptate aute id deserunt nisi.Aliqua id fugiat nostrud irure ex duis ea quis id quis ad e dolore cillum minim tempor enim. Elit aute irure tempor cupidatat incididunt sint deserunt ut voluptate aute id deserunt nisis ea quis id quis ad et. Sunt qui esse pariatur duis deserunt mollit dolore cillum minim tempor enim. Elit aute irure tempor cupidatat incididunt sint deserunt ut voluptate aute id deserunt nisi.Aliqua id fugiat nostrud irure ex duis ea quis id quis ad e dolore cillum minim tempor enim. Elit aute irure tempor cupidatat incididunt sint deserunt ut voluptate aute id deserunt nis cillum minim tempor enim.'
+            `Blanditiis architecto soluta odit itaque eaque, aliquam numquam, libero a praesentium fugit explicabo iste ipsam inventore rem beatae quod, non illum nam.
+            Reiciendis velit reprehenderit libero culpa voluptatibus sint nulla, repellat non numquam repellendus nihil quo. Unde ex praesentium rem officiis? Quo cum soluta autem ducimus modi ad aperiam et nam reiciendis?
+            Fuga, in soluta minima blanditiis eius corporis totam voluptate, fugit iusto dolore quisquam veniam alias ad, reiciendis eum. Ipsa nobis voluptas explicabo iste voluptatem perferendis tempora dolorem enim, voluptates labore.`,
+            
+            `Ex, qui. Sapiente facilis tenetur eaque harum in. Laborum dignissimos quod illum nobis in molestias eum magnam atque, amet, consequatur minus quae.
+            Saepe doloribus assumenda consequatur officiis velit ipsum facere officia voluptate eos ullam iste dolore, provident error perspiciatis totam repudiandae voluptates ut. Alias nesciunt fuga iure ducimus odit est tenetur repellendus?`
         ]
     }
 };
--- scripts/sections/detail.js
+++ scripts/sections/detail.js
@@ -7,9 +7,26 @@ const params = new Proxy(new URLSearchParams(window.location.search), {
   const project = params.project;
   const selectedProject = availableProjects[project];
 
-const detailTitle = document.querySelector('.detail-header h1');
-
+const detailTitle = document.querySelector('.detail-title');
 detailTitle.textContent = selectedProject.title;
+
+const detailArea = document.querySelector('.detail-area');
+detailArea.textContent = selectedProject.area;
+
+const detailDate = document.querySelector('.detail-date');
+detailDate.textContent = selectedProject.completedDate;
+
+const detailImage = document.querySelector('.detail-image');
+detailImage.style.backgroundImage = `url(${selectedProject.backgroundImage})`
+
+const detailDescription = document.querySelector('.detail-description');
+selectedProject.description.forEach(paragraph => {
+    const pTag = document.createElement('p');
+    const textNode = document.createTextNode(paragraph);
+    pTag.appendChild(textNode);
+    detailDescription.appendChild(pTag);
+});
+
 
 
 
